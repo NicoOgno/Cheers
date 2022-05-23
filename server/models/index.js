de24aed66317698e6fa3712ed3f06d2b269c00c6
Nico Ogno <blucode.sur@gmail.com>
2022-05-23 19:41:17 -0400

    Models mongoose database initialized

---
+++ server/models/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv').config();
+const mongoose = require('mongoose');
+const DBURL = process.env.DBURL || `mongodb://localhost:27017/myDatabase`;
+const DBPORT = process.env.DBPORT || 27017;
+
+mongoose.connect(DBURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, (err) => {
+  if (err) {
+    console.log(`Something went wrong at DB connection! ${err}`)
+  } else {
+    console.log(`Database connected at port ${DBPORT}`)
+  }
+})

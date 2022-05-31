a2d937fea81b729785af8529d6e6295f20cc50a8
Nico Ogno <blucode.sur@gmail.com>
2022-05-30 20:46:06 -0400

    Built Pages Home, Register and Login :: routes defined

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './Pages/Home';
+import Register from './Pages/Register';
+import Login from './Pages/Login';
 import './App.css';
 
 function App() {
   return (
-    <div>Hello world</div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+    </Router>
   );
 }
 

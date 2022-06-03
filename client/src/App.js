ff745e017ec79d5d920af3179ec01cc5d5d1ce31
Nico Ogno <blucode.sur@gmail.com>
2022-06-02 20:03:30 -0400

    Component Navbar first draft

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
-        <Route path='/home' element={<Home />} />
+        <Route path='/' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
--- client/src/components/navigation/Navbar.js
+++ client/src/components/navigation/Navbar.js
@@ -1,28 +1,63 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import SportsBarRoundedIcon from '@mui/icons-material/SportsBarRounded';
+import LiquorRoundedIcon from '@mui/icons-material/LiquorRounded';
+import DeckRoundedIcon from '@mui/icons-material/DeckRounded';
+import Button from '@mui/material/Button';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import styles from './navbar.module.css'
 
+const theme = createTheme({
+  status: {
+    danger: '#e53e3e',
+  },
+  palette: {
+    primary: {
+      main: '#4B1FA2',
+      darker: '#053e85',
+    },
+    secondary: {
+      main:'#410039',
+    },
+    neutral: {
+      main: '#64748B',
+      contrastText: '#fff',
+    },
+  },
+});
 
 export default function Navbar() {
+
+  const navigate = useNavigate();
+
   return (
     <nav className={styles.navigation}>
-      <a href="/home" className={styles.brandName}>
-        Cheers
-      </a>
+      <ThemeProvider theme={theme}>
+      <Button color='primary'>
+      <span className={styles.brandName} onClick={()=> {navigate('/')}}>Cheers</span>
+      </Button>
       <div
         className={styles.navigationMenu}>
         <ul>
           <li>
-            <a href="/home">Home</a>
+            <Button color='primary' onClick={() => {navigate('/');}}>
+              <DeckRoundedIcon/><i>Home</i>
+            </Button>
           </li>
           <li>
-            <a href="/login">Login</a>
+            <Button color='primary' onClick={() => {navigate('/login');}}>
+              <SportsBarRoundedIcon/><i>Login</i>
+              </Button>
           </li>
           <li>
-            <a href="/register">Register</a>
+            <Button color='primary' onClick={() => {navigate('/register');}}>
+              <LiquorRoundedIcon/><i>Register</i>
+              </Button>
           </li>
         </ul>
       </div>
+      </ThemeProvider>
     </nav>
   );
 }
